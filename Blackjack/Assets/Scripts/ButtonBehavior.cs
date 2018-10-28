efaddb8fb7f64a7280362ac225569c655320aab1
David Piper <dbpiper@gmail.com>
2018-10-28 11:13:06 -0500

    Make cards show up when deal button is pressed.

---
+++ Blackjack/Assets/Scripts/ButtonBehavior.cs
@@ -0,0 +1,46 @@
+﻿using Boo.Lang;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    public class ButtonBehavior : MonoBehaviour
+    {
+        private static class ButtonTypes
+        {
+            public const string Hit = "Hit Button";
+            public const string Deal = "Deal Button";
+            public const string Stand = "Stand Button";
+        }
+
+        public CardManger CardManagerRef;
+        
+        // Use this for initialization
+        public void Start () {
+        }
+     
+        // Update is called once per frame
+        public void Update () {
+        
+        }
+
+        public void ButtonClicked(GameObject button)
+        {
+             
+            switch (button.name) {
+            case ButtonTypes.Hit:
+                CardManagerRef.Hit();
+                break;
+            case ButtonTypes.Deal:
+                CardManagerRef.Deal();
+                break;
+            case ButtonTypes.Stand:
+                CardManagerRef.Stand();
+                break;
+            default:
+                Debug.Log($"invalid button name {button.name} clicked!");
+                break;
+            }
+        }
+    }
+}
---
+++ Blackjack/Assets/Scripts/CardManger.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class CardManger : MonoBehaviour
+    {
+        public List<GameObject> PlayerCards;
+        public List<GameObject> OpponentCards;
+        // Use this for initialization
+        public void Start () {
+        
+            //foreach (var playerCard in PlayerCards) {
+            //    //Debug.Log("trying to make visible");
+            //    //SpriteRenderer spriteRenderer = playerCard.GetComponent<SpriteRenderer>();
+            //    //spriteRenderer.SetAc
+            //    playerCard.SetActive(false);
+            //}
+        }
+    
+        // Update is called once per frame
+        public void Update () {
+        
+        }
+
+        public void Stand()
+        {
+        }
+
+        public void Deal()
+        {
+            foreach (var playerCard in PlayerCards) {
+                playerCard.SetActive(true);
+            }
+            foreach (var opponentCard in OpponentCards) {
+                opponentCard.SetActive(true);
+            }
+        }
+
+        public void Hit()
+        {
+        }
+    }
+}
